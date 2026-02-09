81664c566426bd93f440661fd33cb65e6057320f
dianingtass <dianingtas@gmail.com>
2026-02-10 00:29:09 +0700

    Keuangan Santri

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from "./pages/Login"
 import SantriDashboard from "./pages/santri/dashboard"
+import SantriProfile from "./pages/santri/pendataan"
+import SantriKeuangan from "./pages/santri/keuangan"
 
 function App() {
   return (
@@ -10,7 +12,11 @@ function App() {
         {/* Public Routes */}
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
+
         <Route path="/santri" element={<SantriDashboard />} />
+        <Route path="/santri/profil" element={<SantriProfile />} />
+        <Route path="/santri/keuangan" element={<SantriKeuangan />} />
+
       </Routes>
     </BrowserRouter>
   );
